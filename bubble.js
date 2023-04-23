cee9b9076a57fa85a79d251b3dc035ef292df4d4
Jeof Petty <jeofpetty@icloud.com>
2023-04-23 17:12:45 -0400

    added bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,20 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i=0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+    return arr;
+}
 
 module.exports = bubbleSort;
